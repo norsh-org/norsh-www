f8e922807311cf52e8939f0c82ee5242e149d25c
Danthur Lice <danthur.lice@norsh.org>
2025-03-18 16:05:15 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,86 +5,141 @@ import { getLangFromPath } from "../i18n";
 import "./Footer.css";
 
 const Footer = () => {
-	const location = useLocation();
-	const lang = getLangFromPath(location.pathname);
+  const location = useLocation();
+  const lang = getLangFromPath(location.pathname);
 
-	return (
-		<>
-			<div className="footer footerContainer" style={{ backgroundImage: `url(${t("cdn")}${t("img.footer.bannerBg")})` }}>
-				<div className="container-lg text-white footerContent">
-					<div className="row w-100">
-						<div className="col-12 col-lg-3 mb-5 pe-lg-5">
-							<img src={`${t("cdn")}${t("img.logo.white")}`} alt="Norsh" height="30" />
+  return (
+    <div
+      className="footer footerContainer"
+      style={{ backgroundImage: `url(${t("cdn")}${t("img.footer.bannerBg")})` }}
+    >
+      <div className="container-lg text-white footerContent">
+        <div className="row w-100">
+          {/* Coluna 1: Logo e Informações */}
+          <div className="col-12 col-lg-3 mb-5 pe-lg-5">
+            <img src={`${t("cdn")}${t("img.logo.white")}`} alt="Norsh" height="30" />
 
-							<p className="mt-4">Designed and built with dedication and innovation by the Norsh team, with the support of our incredible community.</p>
-							<p className="">Licensed under NCL</p>
-							<p>Currently at v1.0.0</p>
-						</div>
+            <p className="mt-4">
+              Designed and built with dedication and innovation by the Norsh team, with the support of our incredible community.
+            </p>
+            <p>Licensed under NCL</p>
+            <p>Currently at v1.0.0</p>
+          </div>
 
-						<div className="col-6 col-lg-3 mb-5">
-							<h5 className="mb-3">Links</h5>
-							<ul className="list-group">
-								<li className="d-inline">
-									<Link to={`/${lang}/home`} className="text-white nav-link mb-3" preventScrollReset>
-										{t("navbar.home")}
-									</Link>
-								</li>
-								{/* <li className="d-inline">
-									<Link to={`/${lang}/about`} className="text-white nav-link mb-3" preventScrollReset>
-										{t("navbar.about")}
-									</Link>
-								</li>
-								<li className="d-inline">
-									<Link to={`/${lang}/contact`} className="text-white nav-link mb-3" preventScrollReset>
-										{t("navbar.contact")}
-									</Link>
-								</li> */}
-							</ul>
-						</div>
+          {/* Coluna 2: Links */}
+          <div className="col-6 col-lg-2 mb-5">
+            <h5 className="mb-3">Links</h5>
+            <ul className="list-group">
+              <li className="d-inline">
+                <Link to={`/${lang}/home`} className="text-white nav-link mb-3">
+                  {t("navbar.home")}
+                </Link>
+              </li>
+              {/* Outros links internos podem ser adicionados aqui */}
+            </ul>
+          </div>
 
-						<div className="col-6 col-lg-3 mb-5">
-							<h5 className="mb-3">Solutions</h5>
-							<ul className="list-group">
-								<li className="d-inline">
-									<Link to={`/${lang}/solutions/hazmat`} className="text-white nav-link mb-3" preventScrollReset>
-										Hazmat
-									</Link>
-								</li>
-								{/* <li className="d-inline">
-									<Link to={`/${lang}/about`} className="text-white nav-link mb-3" preventScrollReset>
-										{t("navbar.about")}
-									</Link>
-								</li>
-								<li className="d-inline">
-									<Link to={`/${lang}/contact`} className="text-white nav-link mb-3" preventScrollReset>
-										{t("navbar.contact")}
-									</Link>
-								</li> */}
-							</ul>
-						</div>
+          {/* Coluna 3: Solutions */}
+          <div className="col-6 col-lg-3 mb-5">
+            <h5 className="mb-3">Solutions</h5>
+            <ul className="list-group">
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/hazmat`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.hazmat")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/pharma`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.pharma")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/suplychain`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.suplychain")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/recycle`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.recycle")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/ip`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.ip")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/solutions/sustainability`} className="text-white nav-link mb-3">
+                  {t("navbarItems.solutions.sustainability")}
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-						<div className="col-12 col-lg-3 mb-5 text-center">
-							<Link to="https://github.com/norsh-org" className="text-white" target="_blank">
-								<IconBrandGithub stroke={2} className="me-3" />
-							</Link>
+          {/* Coluna 4: Programs */}
+          <div className="col-6 col-lg-2 mb-5">
+            <h5 className="mb-3">Programs</h5>
+            <ul className="list-group">
+              <li className="d-inline">
+                <Link to={`/${lang}/developers`} className="text-white nav-link mb-3">
+                  {t("navbarItems.programs.developers")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/byc`} className="text-white nav-link mb-3">
+                  {t("navbarItems.programs.byc")}
+                </Link>
+              </li>
+              <li className="d-inline">
+                <Link to={`/${lang}/govnet`} className="text-white nav-link mb-3">
+                  {t("navbarItems.programs.govnet")}
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-							<Link to="https://www.linkedin.com/company/norsh-org" className="text-white" target="_blank">
-								<IconBrandLinkedin stroke={2} className="me-3" />
-							</Link>
+          {/* Coluna 5: Redes Sociais */}
+          <div className="col-12 col-lg-2 mb-5 text-center">
+            <a
+              href="https://github.com/norsh-org"
+              className="text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconBrandGithub stroke={2} className="me-3" />
+            </a>
 
-							<Link to="https://norsh.medium.com/" className="text-white" target="_blank">
-								<IconBrandMedium stroke={2} className="me-3" />
-							</Link>
+            <a
+              href="https://www.linkedin.com/company/norsh-org"
+              className="text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconBrandLinkedin stroke={2} className="me-3" />
+            </a>
 
-							<Link to="https://x.com/norsh_org" className="text-white" target="_blank">
-								<IconBrandX stroke={2} />
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+            <a
+              href="https://norsh.medium.com/"
+              className="text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconBrandMedium stroke={2} className="me-3" />
+            </a>
+
+            <a
+              href="https://x.com/norsh_org"
+              className="text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconBrandX stroke={2} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
